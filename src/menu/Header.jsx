361c7e86b1eb716f6f646bbc1d02da06f0973026
Daniel Zappala <daniel.zappala@gmail.com>
2024-01-27 10:52:24 -0700

    more notes, plus small fixes

--- src/menu/Header.jsx
+++ src/menu/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header(props) {
   return (
-    <div class="text-xl pt-15 md:text-2xl font-bold">
+    <div className="text-xl pt-15 md:text-2xl font-bold">
       <a href="/">{props.semester}</a>
     </div>
   );
